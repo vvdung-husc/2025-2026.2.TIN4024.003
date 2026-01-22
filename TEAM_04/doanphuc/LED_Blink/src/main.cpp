2185a98b69e730b5d0a1c74d367545c5cd18f1e4
phuctlip <phuckdoan4115@gmail.com>
2026-01-22 09:09:50 +0700

    LED_Blink

--- TEAM_04/doanphuc/LED_Blink/src/main.cpp
+++ TEAM_04/doanphuc/LED_Blink/src/main.cpp
@@ -1,25 +1,39 @@
 #include <Arduino.h>
 
-// Định nghĩa chân số 5 theo như sơ đồ dây nối của bạn
-#define LED_PIN 5 
+// Định nghĩa các chân cắm dựa trên sơ đồ của bạn
+#define LED_GREEN  19
+#define LED_YELLOW 18
+#define LED_RED    5
 
 void setup() {
-  // Khởi tạo cổng Serial để xem log (thay cho printf trong Arduino)
   Serial.begin(115200);
-  Serial.println("Welcome IoT");
+  Serial.println("He thong den giao thong dang khoi dong...");
 
-  // Cấu hình chân LED_PIN là đầu ra
-  pinMode(LED_PIN, OUTPUT);
+  // Cấu hình tất cả các chân là OUTPUT
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
 }
 
 void loop() {
-  // Bật đèn LED
-  Serial.println("[LED_RED]_HIGH");
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000); // Đợi 1 giây (1000ms)
+  // 1. ĐÈN XANH SÁNG (Ví dụ: 5 giây)
+  Serial.println("DEN XANH");
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_RED, LOW);
+  delay(5000); 
 
-  // Tắt đèn LED
-  Serial.println("[LED_RED]_LOW");
-  digitalWrite(LED_PIN, LOW);
-  delay(1000); // Đợi 1 giây
+  // 2. ĐÈN VÀNG SÁNG (Ví dụ: 2 giây)
+  Serial.println("DEN VANG");
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  digitalWrite(LED_RED, LOW);
+  delay(2000);
+
+  // 3. ĐÈN ĐỎ SÁNG (Ví dụ: 5 giây)
+  Serial.println("DEN DO");
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_RED, HIGH);
+  delay(5000);
 }
