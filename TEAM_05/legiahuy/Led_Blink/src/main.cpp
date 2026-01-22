bc89e5edcb395d4e85b9460101bc7cf9dcdf4fd5
Gia Huy <legiahuy4002@gmail.com>
2026-01-22 09:50:15 +0700

    led_blink_dengiaothong

--- TEAM_05/legiahuy/Led_Blink/src/main.cpp
+++ TEAM_05/legiahuy/Led_Blink/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int RED = 2;
+const int YELLOW = 4;
+const int GREEN = 5;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Đèn Đỏ sáng
+  Serial.println("DUNG LAI - DEN DO");
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, LOW);
+  delay(5000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Đèn Xanh sáng
+  Serial.println("DI TIEP - DEN XANH");
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, HIGH);
+  delay(5000);
+
+  // Đèn Vàng sáng
+  Serial.println("CHUAN BI DUNG - DEN VANG");
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(GREEN, LOW);
+  delay(2000);
 }
