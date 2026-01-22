d27f54267a3c5e40f92628ae4a197d0d5dd8a3f3
phuctlip <phuckdoan4115@gmail.com>
2026-01-22 08:55:14 +0700

    LED_Blink

--- TEAM_04/doanphuc/LED_Blink/src/main.cpp
+++ TEAM_04/doanphuc/LED_Blink/src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
 
+// Định nghĩa chân số 5 theo như sơ đồ dây nối của bạn
+#define LED_PIN 5 
 
 void setup() {
-  printf("welcome IoT");
+  // Khởi tạo cổng Serial để xem log (thay cho printf trong Arduino)
+  Serial.begin(115200);
+  Serial.println("Welcome IoT");
+
+  // Cấu hình chân LED_PIN là đầu ra
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  printf("loop() running ");
-  sleep(1);
+  // Bật đèn LED
+  Serial.println("[LED_RED]_HIGH");
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000); // Đợi 1 giây (1000ms)
+
+  // Tắt đèn LED
+  Serial.println("[LED_RED]_LOW");
+  digitalWrite(LED_PIN, LOW);
+  delay(1000); // Đợi 1 giây
 }
