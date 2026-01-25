83d0ac7d31946305e9b05e7033ad1a26ba814fac
lvminh1912 <levanminh19122004@gmail.com>
2026-01-26 02:05:46 +0700

    update

--- TEAM_12/LeVanMinh/LED_Blink/src/main.cpp
+++ TEAM_12/LeVanMinh/LED_Blink/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RED_LED    17
+#define GREEN_LED  16
+#define YELLOW_LED 25
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  delay(4000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(GREEN_LED, LOW);
+    delay(300);
+    digitalWrite(GREEN_LED, HIGH);
+    delay(300);
+  }
+
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(RED_LED, LOW);
+  delay(2000);
+
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, HIGH);
+  delay(5000);
 }
