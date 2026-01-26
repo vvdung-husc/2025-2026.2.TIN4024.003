425837eca9ea47383114b07d02f5da40d623848c
phatchutich1211 <phatdang121104@gmail.com>
2026-01-26 09:33:00 +0700

    Hoan thanh mo phong ESP32 den nhap nhay

--- TEAM_11/DangTanPhat/DENNHAPNHAY/src/main.cpp
+++ TEAM_11/DangTanPhat/DENNHAPNHAY/src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 2   // LED onboard ESP32
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
 }
