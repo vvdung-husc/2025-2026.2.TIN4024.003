eb4b346d8c6a69fb66e2bb7837f2eee04eb32071
djwin2609 <nguyenhoangthienphuc09@gmail.com>
2026-01-28 21:26:07 +0700

    Den giao thong

--- TEAM_06/NguyenHoangThienPhuc/Led_Blink/src/main.cpp
+++ TEAM_06/NguyenHoangThienPhuc/Led_Blink/src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 
-int leds[] = {2, 26, 27};
+int leds[] = {25, 26, 27};
+int n = 3;
 
 void setup() {
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < n; i++) {
     pinMode(leds[i], OUTPUT);
   }
 }
 
 void loop() {
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < n; i++) {
     digitalWrite(leds[i], HIGH);
     delay(500);
     digitalWrite(leds[i], LOW);
