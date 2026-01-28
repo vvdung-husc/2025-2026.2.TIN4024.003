26ff65c51b953962997ce4a03ca20deb6b337349
phatdang121104 <phatdang121104@gmailcom>
2026-01-29 00:57:43 +0700

    update DANGTANPHAT

--- TEAM_11/DangTanPhat/DENNHAPNHAY/src/main.cpp
+++ TEAM_11/DangTanPhat/DENNHAPNHAY/src/main.cpp
@@ -1,35 +1,24 @@
 #include <Arduino.h>
-#define LED_RED    25
-#define LED_YELLOW 26
-#define LED_GREEN  27
+#define LED_RED     26
+#define LED_YELLOW  25
+#define LED_GREEN   33
 
 void setup() {
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
-
-  Serial.begin(115200);
 }
 
 void loop() {
-  // Đèn xanh
-  digitalWrite(LED_GREEN, HIGH);
-  digitalWrite(LED_YELLOW, LOW);
-  digitalWrite(LED_RED, LOW);
-  Serial.println("LED [GREEN] ON -> 7 Seconds");
-  delay(7000);
-
-  // Đèn vàng
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_YELLOW, HIGH);
-  digitalWrite(LED_RED, LOW);
-  Serial.println("LED [YELLOW] ON -> 3 Seconds");
-  delay(3000);
-
-  // Đèn đỏ
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_YELLOW, LOW);
   digitalWrite(LED_RED, HIGH);
-  Serial.println("LED [RED] ON -> 5 Seconds");
   delay(5000);
+  digitalWrite(LED_RED, LOW);
+
+  digitalWrite(LED_YELLOW, HIGH);
+  delay(3000);
+  digitalWrite(LED_YELLOW, LOW);
+
+  digitalWrite(LED_GREEN, HIGH);
+  delay(7000);
+  digitalWrite(LED_GREEN, LOW);
 }
