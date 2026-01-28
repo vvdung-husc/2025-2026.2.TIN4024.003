303b97b2cc883ba0659db38d911305c0b9776935
phatdang121104 <phatdang121104@gmailcom>
2026-01-29 00:56:56 +0700

    update

--- TEAM_11/DangTanPhat/DENNHAPNHAY/src/main.cpp
+++ TEAM_11/DangTanPhat/DENNHAPNHAY/src/main.cpp
@@ -1,14 +1,35 @@
 #include <Arduino.h>
-
-#define LED_PIN 2   // LED onboard ESP32
+#define LED_RED    25
+#define LED_YELLOW 26
+#define LED_GREEN  27
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+
+  Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  // Đèn xanh
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_RED, LOW);
+  Serial.println("LED [GREEN] ON -> 7 Seconds");
+  delay(7000);
+
+  // Đèn vàng
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  digitalWrite(LED_RED, LOW);
+  Serial.println("LED [YELLOW] ON -> 3 Seconds");
+  delay(3000);
+
+  // Đèn đỏ
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_RED, HIGH);
+  Serial.println("LED [RED] ON -> 5 Seconds");
+  delay(5000);
 }
