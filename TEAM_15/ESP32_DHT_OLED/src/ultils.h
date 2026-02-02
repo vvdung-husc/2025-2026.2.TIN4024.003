07a50d552872a1312164be621f0c5cad68994717
Le Tang Phuoc <phuocletang1@gmail.com>
2026-02-02 08:42:15 +0700

    nhom15

---
+++ TEAM_15/ESP32_DHT_OLED/src/ultils.h
@@ -0,0 +1,14 @@
+/*
+Thông tin nhóm 15:
+1. Lê Tăng Phước
+*/
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "main.h"
+
+void showOLED(float temp, float hum);
+void updateLED(float temp);
+
+#endif
