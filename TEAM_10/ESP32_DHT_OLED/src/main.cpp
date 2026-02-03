7ae9cdb4fb82b8caa1645a2ab0063ed51c7cf00b
dinhtuananh9876 <adinhdell@gmail.com>
2026-02-03 21:07:29 +0700

    dinhtuananh_hienthiman_oled

--- TEAM_10/ESP32_DHT_OLED/src/main.cpp
+++ TEAM_10/ESP32_DHT_OLED/src/main.cpp
@@ -110,5 +110,38 @@ void loop() {
     NhapNhayLED();
 
     // Hiển thị thông tin lên OLED viet tiep
+    if (isnan(nhiet_do) || isnan(do_am)) {
+      Serial.println("DHT error");
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.print("DHT Error!");
+      display.display();
+      chon_LED = -1;
+      return; 
+    }
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.print("Temperature:");
+    display.print(thongtin); 
+    display.setTextSize(2);
+    display.setCursor(0,15);
+    display.print(nhiet_do, 1);
+    display.print(" C");
+
+    display.setTextSize(1);
+    display.setCursor(0,40);
+    display.print("Humidity:");
+    
+    display.setTextSize(2);
+    display.setCursor(0,50);
+    display.print(do_am, 1);
+    display.print(" %");
+
+    display.display();
+
+    Serial.print("Temp: "); Serial.print(nhiet_do);
+    Serial.print(" - LED Pin: "); Serial.println(chon_LED);
   }
 }
