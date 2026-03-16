55061c502dc3a0b9741904a92473ff39bf6982b7
thngoc0000 <ngoctr2k4@gamil.com>
2026-03-16 07:44:09 +0700

    ESP8266_LED update

--- TEAM_08/TruongHuuNgoc/ESP8266_LED/src/main.cpp
+++ TEAM_08/TruongHuuNgoc/ESP8266_LED/src/main.cpp
@@ -10,19 +10,13 @@
 #define LED_PIN 2
 #define BTN_PIN 12
 
-// ===== OBJECT =====
 DHT dht(DHTPIN, DHTTYPE);
 
 // OLED SH1106
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
-// ===== BIẾN HỆ THỐNG =====
 unsigned long currentMiliseconds = 0;
-unsigned long uptimeSeconds = 0;
 
-// bool counterActive = true;
-
-// ===== HÀM CHỐNG DỘI =====
 bool IsReady(unsigned long &ulTimer, uint32_t milisecond)
 {
   if (currentMiliseconds - ulTimer < milisecond)
@@ -32,54 +26,20 @@ bool IsReady(unsigned long &ulTimer, uint32_t milisecond)
   return true;
 }
 
-// ===== XỬ LÝ NÚT =====
-// void updateButton()
-// {
-//   static unsigned long lastTime = 0;
-//   static int lastValue = HIGH;
-
-//   if (!IsReady(lastTime, 50))
-//     return;
-
-//   int v = digitalRead(BTN_PIN);
-
-//   if (v == lastValue)
-//     return;
-
-//   lastValue = v;
-
-//   if (v == LOW)
-//     return;
-
-//   counterActive = !counterActive;
-
-//   if (counterActive)
-//     Serial.println("SYSTEM ON");
-//   else
-//     Serial.println("SYSTEM OFF");
-// }
-
-// ===== LED NHẤP NHÁY =====
+// ===== LED =====
 void blinkLED()
 {
   static unsigned long lastTime = 0;
   static bool ledState = false;
 
-  // if (!counterActive)
-  // {
-  //   digitalWrite(LED_PIN, LOW);
-  //   return;
-  // }
-
   if (!IsReady(lastTime, 1000))
     return;
 
   ledState = !ledState;
-
   digitalWrite(LED_PIN, ledState);
 }
 
-// ===== ĐỌC CẢM BIẾN =====
+// ===== SENSOR =====
 void readSensor()
 {
   static unsigned long lastTime = 0;
@@ -90,32 +50,32 @@ void readSensor()
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
-  if (isnan(h) || isnan(t))
-  {
-    Serial.println("DHT ERROR");
-    return;
-  }
-
-  Serial.print("Temp: ");
-  Serial.println(t);
-
-  Serial.print("Hum: ");
-  Serial.println(h);
-
-  // HIỂN THỊ OLED
   u8g2.clearBuffer();
-
   u8g2.setFont(u8g2_font_ncenB08_tr);
 
   u8g2.drawStr(0, 15, "ESP8266 SENSOR");
 
-  char tempStr[20];
-  sprintf(tempStr, "Temp: %.1f C", t);
-  u8g2.drawStr(0, 35, tempStr);
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println("DHT ERROR");
+    u8g2.drawStr(0, 40, "Sensor Error");
+  }
+  else
+  {
+    Serial.print("Temp: ");
+    Serial.println(t);
 
-  char humStr[20];
-  sprintf(humStr, "Hum : %.1f %%", h);
-  u8g2.drawStr(0, 55, humStr);
+    Serial.print("Hum: ");
+    Serial.println(h);
+
+    char tempStr[20];
+    sprintf(tempStr, "Temp: %.1f C", t);
+    u8g2.drawStr(0, 35, tempStr);
+
+    char humStr[20];
+    sprintf(humStr, "Hum : %.1f %%", h);
+    u8g2.drawStr(0, 55, humStr);
+  }
 
   u8g2.sendBuffer();
 }
@@ -129,6 +89,8 @@ void setup()
   pinMode(BTN_PIN, INPUT_PULLUP);
 
   dht.begin();
+  delay(2000);
+
   u8g2.begin();
 
   Serial.println("SYSTEM READY");
@@ -139,7 +101,6 @@ void loop()
 {
   currentMiliseconds = millis();
 
-  // updateButton();
   blinkLED();
   readSensor();
 }
