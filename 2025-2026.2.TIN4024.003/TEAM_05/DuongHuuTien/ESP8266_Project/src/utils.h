ad124881ea6d8472f7ad991dffe09c6bff2bcca7
TrungSky1404z <htrung1404@gmail.com>
2026-03-26 19:04:34 +0700

    trung

--- 2025-2026.2.TIN4024.003/TEAM_05/DuongHuuTien/ESP8266_Project/src/utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool IsReady(unsigned long &ulTimer, uint32_t millisecond);
-String StringFormat(const char* fmt, ...);
--- TEAM_11/DangTanPhat/TELEGRAM_LED/src/secret.h
+++
@@ -1,7 +0,0 @@
-#ifndef SECRET_H
-#define SECRET_H
-
-#define BOT_TOKEN "8784001653:AAGDXo4gPTkycTGcpjQkt1vTwvFf5ZF_KBU"  
-#define CHAT_ID "5258732023" 
-
-#endif
