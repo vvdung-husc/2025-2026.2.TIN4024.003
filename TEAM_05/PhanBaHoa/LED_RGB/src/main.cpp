3c823e144e7c93f5ce39195282b245a1f4e55698
22t1020119-hub <22t1020119@husc.edu.vn>
2026-04-05 22:09:36 +0700

    pbh_tieuLuan

--- TEAM_05/PhanBaHoa/LED_RGB/src/main.cpp
+++ TEAM_05/PhanBaHoa/LED_RGB/src/main.cpp
@@ -1,8 +1,3 @@
-/*
- * Dự án: Điều khiển LED RGB qua Blynk Web
- * Tác giả: Phan Bá Hóa
- */
-
 // --- 3 DÒNG NÀY PHẢI Ở TRÊN CÙNG ---
 #define BLYNK_TEMPLATE_ID "TMPL6pK8E6c7E"
 #define BLYNK_TEMPLATE_NAME "LED RGB SMART"
